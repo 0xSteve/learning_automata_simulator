97e4d80bbd5b0b5ad03d51b72083174402f183f6
0xSteve <steven.porretta@gmail.com>
2017-03-20 10:27:15 -0400

    Add a module to contain learning automata with an empty function for the Tsetlin automata.

---
+++ learning_automata.py
@@ -0,0 +1,18 @@
+# Learning_automata.py is a module containing some learning automata.
+# For now it only contains a Tsetlin 2N,2 automaton.
+
+# Write a Tsetlin automaton.
+# A Tsetlin automaton is defined by N memory states, R actions,
+# and c penalties.
+#
+# N is expected to be a whole number indicating memory depth.
+# R is expected to be a while number indicating the number of,
+# actions.
+# c is a vector quantity indicating the penalties for actions
+# 1 to i.
+
+
+def tsetlin(N, R, c):
+    '''Tsetlin automata. N, R positive integers. c a structure
+       of penalties from action 1 to action i.'''
+    pass
