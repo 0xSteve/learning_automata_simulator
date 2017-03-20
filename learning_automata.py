5367f43e22483aa5ddf3de6e03db671e4fa76709
0xSteve <steven.porretta@gmail.com>
2017-03-20 17:17:16 -0400

    Added functionality to next state on reward / penalty. Fixed starting state to match a T2N,R machine.

--- learning_automata.py
+++ learning_automata.py
@@ -19,7 +19,8 @@ from random import random
 
 class Tsetlin(object):
     '''Tsetlin automata. N, R positive integers. c a structure
-       of penalties from action 1 to action i.'''
+       of penalties from action 1 to action i. First state starts at 1 not
+       0.'''
 
     def __init__(self, N, R, c):
         '''initialize a Tsetlin automaton with N memory states,
@@ -28,7 +29,7 @@ class Tsetlin(object):
         self.R = R
         self.c = c
         # define the initial state as the state leaning toward action one.
-        self.current_state = self.N / 2
+        self.current_state = self.N / R
 
     # Find the next state given that the teacher rewarded.
     def next_state_on_reward(self):
@@ -40,15 +41,23 @@ class Tsetlin(object):
         # If the current state is <= 2N/R in action 1
         # if 2N/R < current state < 2(2N/R) in action 2
 
-        if(self.current_state > 0):
+        if (self.current_state % (2 * self.N / self.R) == 0):
+            self.current_state -= 2 * self.N / self.R
+        elif(self.current_state - (2 * self.N / self.R) != 1):
             self.current_state -= 1
 
     # Find the next state given that the teacher penalized.
     def next_state_on_penalty(self):
         '''Find the next state of the learner, given that the teacher
            penalized.'''
-        if(self.current_state < self.N):
+
+        # The hard part here is not looping back, but instead jumping to
+        # the next 2N / R state.
+
+        if(self.current_state % (2 * self.N / self.R) != 0):
             self.current_state += 1
+        else:
+            self.current_state += 2 * self.N / self.R
 
     # Determine the next state as the teacher.
     def environment_response(self):
