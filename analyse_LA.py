a7fc5a3360942ccf20faa44cd0381a95fa11db34
0xSteve <steven.porretta@gmail.com>
2017-03-22 09:26:12 -0400

    Added a very basic stationary probability function for the analytic stationary probability calculator.  It only considers the requirement for assignment 4.

--- analyse_LA.py
+++ analyse_LA.py
@@ -6,7 +6,10 @@
 
 class Tsetlin(object):
     '''Analysis tools for the Tsetlin automaton.'''
-    def stationary_probability(self, is_analytic):
+    def stationary_probability(self, is_analytic, c):
+        '''This only assumes reasonable functionality for a two action
+           automaton, due to time constraints.'''
+
         probability = 0
         if(is_analytic):
             probability = self.stationary_probability_analytic()
@@ -15,8 +18,18 @@ class Tsetlin(object):
 
         return probability
 
-    def stationary_probability_analytic(self):
-        pass
+    def stationary_probability_analytic(self, c, N):
+        '''This computes the exact stationary probability, iff the
+           automaton is 2 action.  Since the requirement is only for p1
+           at infinity, then p2 is discarded, due to time constraints.'''
+        N = N / 2  # Assume that N has been doubled to 2N.
+        d1 = 1 - c[0]
+        d2 = 1 - c[1]
+        term1 = pow(c[0] / c[1], N) * ((c[0] * d1) / (c[1] * d2))
+        term2 = (pow(c[0], N) - pow(d1, N)) / (pow(c[1], N) - pow(d2, N))
+        p1_inf = 1 / [1 + term1 * term2]
+
+        return p1_inf
 
     def stationary_probability_estimate(self):
         pass
