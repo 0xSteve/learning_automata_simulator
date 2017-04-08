0db45d2c51a907a22d71ae7b4f7565d26ff3edf4
0xSteve <steven.porretta@gmail.com>
2017-04-08 13:16:20 -0400

    Added a helper to create a penalty probability vector.

--- helpers.py
+++ helpers.py
@@ -1,2 +1,15 @@
+'''Some helper functions.'''
+
+
 def make_p(count):
+    '''A helper function that generates count based on the number of
+    actions.'''
+    a = []
+    for i in range(count):
+        a.append(1 / count)
+    return a
+
+
+def make_c(count):
+    # I might need this, who knows?
     pass
